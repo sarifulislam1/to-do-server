6af9d17e91b9eb6417de219417ae429ac842f935
Your Name <sarifulislaml8280@gmail.com>
2022-05-19 01:48:15 +0600

    delete api done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wwril.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -24,6 +24,26 @@ async function run() {
             const data = req.body
             const result = await taskCollection.insertOne(data)
 
+            res.send(result)
+        })
+
+        // get api
+        app.get('/addTask', async (req, res) => {
+
+            const q = req.query
+            const cursor = taskCollection.find(q)
+            const result = await cursor.toArray()
+
+            res.send(result)
+
+        })
+
+        //delet api 
+        app.delete('/addTask/:id', (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+
+            const result = taskCollection.deleteOne(filter)
             res.send(result)
         })
 
