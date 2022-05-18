d47d86f8b75390e74d94efd2ccd4e91be2dab8a1
Your Name <sarifulislaml8280@gmail.com>
2022-05-19 00:27:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 4000
+const cors = require("cors")
+
+require('dotenv').config()
+app.use(express.json());
+app.use(cors());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wwril.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db("tasksInfo").collection("tasks");
+
+    } finally {
+
+    }
+}
+
+
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
